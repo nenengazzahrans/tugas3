9379174f89b0b2c254d7c056dc8c46276883357c
nenengazzahrans <zara.azzahrans@gmail.com>
2018-04-05 18:57:08 +0700

    v-01

---
+++ tugas3.py
@@ -0,0 +1,152 @@
+
+
+Python 3.4.2 (v3.4.2:ab2c023a9432, Oct  6 2014, 22:16:31) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+>>> 
+
+Username : neneng.azzahrans
+
+Warning (from warnings module):
+  File "C:\Python34\lib\getpass.py", line 101
+    return fallback_getpass(prompt, stream)
+GetPassWarning: Can not control echo on the terminal.
+Warning: Password input may be echoed.
+Password: neaz20
+===================================================
+=====================================================
+
+	- - -pilihan- - -
+	1. penilaian mahasiswa
+	2. pembayaran mahasiswa
+
+	silahkan pilih : 1
+Masukan Nama: azzahrans
+Masukan Nim: 31171999
+	Nilai Tugas: 100
+	Nilai UTS: 90
+	Nilai UAS: 95
+
+	Tambah data[y/n]? y
+Masukan Nama: aseptian
+Masukan Nim: 31171995
+	Nilai Tugas: 90
+	Nilai UTS: 95
+	Nilai UAS: 80
+
+	Tambah data[y/n]? y
+Masukan Nama: azkia
+Masukan Nim: 31172014
+	Nilai Tugas: 100
+	Nilai UTS: 80
+	Nilai UAS: 85
+
+	Tambah data[y/n]? n
++----+-----------+----------+-------------+-----------+-----------+------------+
+| No |   Nama    |   Nim    | Nilai Tugas | Nilai UTS | Nilai UAS |   Nilai    |
+|    |           |          |             |           |           |   Akhir    |
++====+===========+==========+=============+===========+===========+============+
+| 1  | azzahrans | 31171999 |         100 |        90 |        95 |         95 |
++----+-----------+----------+-------------+-----------+-----------+------------+
+| 2  | aseptian  | 31171995 |          90 |        95 |        80 |     88.333 |
++----+-----------+----------+-------------+-----------+-----------+------------+
+| 3  | azkia     | 31172014 |         100 |        80 |        85 |     88.333 |
++----+-----------+----------+-------------+-----------+-----------+------------+
+nKembali ke menu (y/t)? y
+=====================================================
+
+	- - -pilihan- - -
+	1. penilaian mahasiswa
+	2. pembayaran mahasiswa
+
+	silahkan pilih : 2
+
+===============================================
+
+	masukan nama = zara
+	masukan nim = 31171998
+	masukan semester saat ini = 1
+
+	- - -pilihan pembayaran- - -
+	1. pembayaran spp
+	2. pembayaran uts
+	3. pembayaran uas
+	4. pembayaran spp & uts
+	5. pembayaran spp & uas
+
+	silahkan pilih : 2
+
+	jumlah mata kuliah = 5
+============================================================
+	total bayar uts Rp.40000* 5.0  = Rp. 200000.0
+nKembali ke menu (y/t)? y
+=====================================================
+
+	- - -pilihan- - -
+	1. penilaian mahasiswa
+	2. pembayaran mahasiswa
+
+	silahkan pilih : 2
+
+===============================================
+
+	masukan nama = azzahrans
+	masukan nim = 31171999
+	masukan semester saat ini = 1
+
+	- - -pilihan pembayaran- - -
+	1. pembayaran spp
+	2. pembayaran uts
+	3. pembayaran uas
+	4. pembayaran spp & uts
+	5. pembayaran spp & uas
+
+	silahkan pilih : 5
+
+	jumlah bulan yang di bayar = 3
+
+	jumlah mata kuliah = 5
+
+	total bayar spp Rp.500000 *  3.0  = Rp. 1500000.0
+	total bayar uas Rp.50000* 5.0  = Rp. 250000.0
+	biaya tambahan server sebesar Rp. 5000
+====================================================================
+	total yang harus dibayar 1755000.0
+nKembali ke menu (y/t)? y
+=====================================================
+
+	- - -pilihan- - -
+	1. penilaian mahasiswa
+	2. pembayaran mahasiswa
+
+	silahkan pilih : y
+nKembali ke menu (y/t)? y
+=====================================================
+
+	- - -pilihan- - -
+	1. penilaian mahasiswa
+	2. pembayaran mahasiswa
+
+	silahkan pilih : 2
+
+===============================================
+
+	masukan nama = zara
+	masukan nim = 31171998
+	masukan semester saat ini = 1
+
+	- - -pilihan pembayaran- - -
+	1. pembayaran spp
+	2. pembayaran uts
+	3. pembayaran uas
+	4. pembayaran spp & uts
+	5. pembayaran spp & uas
+
+	silahkan pilih : 1
+
+	jumlah bulan yang di bayar = 4
+============================================================
+	total bayar spp Rp.500000 * 4.0  = Rp. 2000000.0
+nKembali ke menu (y/t)? t
+>>> 
